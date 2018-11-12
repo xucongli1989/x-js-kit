612fa0561ad14140429baab3b0d534019d0c6c29
xucl <xucl@Ctrip.com>
2018-11-12 13:07:52 +0800

    unittest

--- test/cache.test.ts
+++ test/cache.test.ts
@@ -1,14 +1,33 @@
 import cache from "../src/cache/index"
 import { ItemContentType } from "../src/cache/localStorage"
 
-test('cache.localStorage', () => {
+const cacheName = "myCache"
+
+test('cache.localStorage-添加与获取', () => {
     const itemContent: ItemContentType = {
         value: "123456"
     }
-    //添加与获取
-    cache.localStorage.add("myCache", itemContent)
-    expect(cache.localStorage.get("myCache")).toEqual(itemContent)
-    //添加与获取（带过期时间）
-    itemContent.expire = new Date().valueOf() - 1
-    //expect(cache.localStorage.get("myCache")).toEqual(null)
-});
+    cache.localStorage.add(cacheName, itemContent)
+    expect(cache.localStorage.get(cacheName)).toEqual(itemContent)
+})
+
+test('cache.localStorage-添加与获取（初始化为过期时间）', () => {
+    const itemContent: ItemContentType = {
+        value: "123456",
+        expire: new Date().valueOf() - 1
+    }
+    cache.localStorage.add(cacheName, itemContent)
+    expect(cache.localStorage.get(cacheName)).toEqual(null)
+})
+
+test('cache.localStorage-添加与获取（定时为过期时间）', async () => {
+    const itemContent: ItemContentType = {
+        value: "123456",
+        expire: new Date().valueOf() + 1000
+    }
+    cache.localStorage.add(cacheName, itemContent)
+    await new Promise(res => {
+        setTimeout(res, 1001)
+    })
+    expect(cache.localStorage.get(cacheName)).toEqual(null)
+})
