df7cba14ec3b6eb37fa119b15b9b1487a13a29b9
xucongli1989 <80213876@qq.com>
2018-11-21 15:02:55 +0800

    update

--- src/pattern/strategy.ts
+++ src/pattern/strategy.ts
@@ -1,27 +1,40 @@
 /**
  * 策略项
  */
-export class StrategyItem {
+class StrategyItem {
+    constructor(name: string, context: any, handler: (item: StrategyItem) => void) {
+        this.name = name
+        this.context = context
+        this.handler = handler
+    }
     name: string = ""
     context: any
-    handler(item: StrategyItem) { }
+    handler(item: StrategyItem): void { }
 }
 
 /**
  * 策略管理类
  */
 export class Strategy {
+    /**
+     * 策略列表
+     */
     private _strategyList: StrategyItem[] = []
+    /**
+     * 上下文，用于多个策略之间的数据共享
+     */
     context: any
+    /**
+     * 添加一个策略项 
+     */
     add(name: string, handler: (item: StrategyItem) => void) {
-        this._strategyList.push({
-            name: name,
-            context: this.context,
-            handler: handler
-        })
+        this._strategyList.push(new StrategyItem(name, this.context, handler))
         return this
     }
-    process() {
+    /**
+     * 执行策略列表
+     */
+    execute() {
         this._strategyList.forEach(item => {
             item.handler(item)
         })
