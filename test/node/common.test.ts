a34f5a5d8f1e037921473c3d95b2030211dbfaad
xucongli1989 <80213876@qq.com>
2018-12-10 19:47:29 +0800

    update test

--- test/node/common.test.ts
+++ test/node/common.test.ts
@@ -2,15 +2,14 @@
  * 这一行规定此文件要在node环境下运行，需要保持在文件的最开头，因为后面的代码都需要在node环境中运行了
  * @jest-environment node
  */
-import common from "../../src/common/index"
 
-const localStorage = require("localStorage")
 Object.assign(global, {
-    localStorage
+    localStorage: require("localStorage")
 })
+const kit = require("../../dist/index").default
 
 test("common.lib", () => {
-    expect(common.lib.getLocalStorage()).toBeDefined()
-    expect(common.lib.isBowser()).toBeFalsy()
-    expect(common.lib.isServer()).toBeTruthy()
+    expect(kit.common.lib.getLocalStorage()).toBeDefined()
+    expect(kit.common.lib.isBowser()).toBeFalsy()
+    expect(kit.common.lib.isServer()).toBeTruthy()
 })
