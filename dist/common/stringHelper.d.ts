21298f88ac091d828a49489da4d764efc13dae37
xucongli1989 <80213876@qq.com>
2018-12-13 22:13:59 +0800

    add string ellipsis method

--- dist/common/stringHelper.d.ts
+++ dist/common/stringHelper.d.ts
@@ -2,4 +2,11 @@
  * 将字符串按一定字符数拆分成字符串数组
  */
 export declare const splitString: (str: string, stepCharCount: number) => string[];
+/**
+ * 截取字符串并以省略符号显示字符串
+ * @param str 原字符串
+ * @param len 要保留的字符长度
+ * @param ellipsisChars 被截断的字符显示的符号
+ */
+export declare const ellipsis: (str: string, len: number, ellipsisChars?: string) => string;
 //# sourceMappingURL=stringHelper.d.ts.map
