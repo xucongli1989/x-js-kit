2feb120a812f1625cb85dce617510da56cfff388
xucl <xucl@Ctrip.com>
2019-01-02 19:11:00 +0800

    update

--- src/pattern/singleton.ts
+++ src/pattern/singleton.ts
@@ -5,12 +5,9 @@ const classIdSymbol = Symbol()
  * @param className 类的名字
  */
 export const getInstance = <T>(className: new () => T): T => {
-    const symList = Object.getOwnPropertySymbols(className)
-    if (symList && symList.length) {
-        const obj = symList.find(k => k === classIdSymbol)
-        if (obj) {
-            return (className as any)[obj] as T
-        }
+    const obj = (className as any)[classIdSymbol] as T
+    if (typeof (obj) != 'undefined') {
+        return obj
     }
     const newObj = new className()
     Object.defineProperty(className, classIdSymbol, {
