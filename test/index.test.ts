5f6ac5824a39d256ccd99e6219658db30e2b5cfa
xucongli1989 <80213876@qq.com>
2019-01-25 10:28:27 +0800

    feat(lib): use babel when output lib folder

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,22 @@
 import JsKit from "../dist/index"
+import JsKitLib from "../lib/index"
+import stopWatch from "../lib/timer/stopWatch"
 
 test("index-引用测试", () => {
     const obj = JsKit.cache.localStorage.getGlobalCache()
     expect(obj).toBeDefined()
+})
+
+test("index-引用测试(lib目录)", () => {
+    const obj = JsKitLib.cache.localStorage.getGlobalCache()
+    expect(obj).toBeDefined()
+})
+
+test("引用部分模块测试", async () => {
+    const sw = new stopWatch()
+    sw.start()
+    await new Promise(res => {
+        setTimeout(res, 10)
+    })
+    expect(sw.stop().value).toBeGreaterThan(0)
 })
