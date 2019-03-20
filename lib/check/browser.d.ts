f5bfc591823fa81c66ddb92632217c2b7ca67a74
xucongli1989 <80213876@qq.com>
2019-03-20 13:40:47 +0800

    feat(check.browser): add 'isIE' method

---
+++ lib/check/browser.d.ts
@@ -0,0 +1,7 @@
+/**
+ * 判断是否为IE浏览器（仅支持<=IE 11）
+ * @param version 具体的IE版本号
+ * @param userAgent 浏览器代理字符串，若不指定，则取navigator.userAgent
+ */
+export declare function isIE(version?: number, userAgent?: string): boolean;
+//# sourceMappingURL=browser.d.ts.map
