0a84534e165022d6d6ffedf38f5d40940d944510
xucongli1989 <80213876@qq.com>
2020-04-08 08:56:14 +0800

    test(test): temp test code

--- src/recorder/dom.ts
+++ src/recorder/dom.ts
@@ -2,14 +2,41 @@ import { document, globalObject } from "../common/lib"
 import { AnyKeyValueType } from "../declaration/common"
 
 /**
+ * key 的枚举值
+ */
+enum KeyTypeEnum {
+    /**
+     * 记录分组
+     */
+    group = "group",
+    /**
+     * 记录属性值
+     */
+    val = "val",
+    /**
+     * 记录innerText
+     */
+    txt = "txt",
+    /**
+     * 记录innerHTML
+     */
+    html = "html",
+    /**
+     * 记录次数
+     */
+    count = "count"
+}
+
+
+/**
  * DOM数据收集记录器类。用于批量收集DOM元素的属性信息，并将这些属性内容存放到一个对象中。
- * 示例：<div jskit-key="name" jskit-val="test"></div>，则最终会把【name】与【test】保存到一个对象中。
+ * 示例：<div jskit-key="name" jskit-type="val" jskit-val="test"></div>，则最终会把【name】与【test】保存到一个对象中。
  * 说明：jskit-key属性为此数据的标识key，value的值为：
- * 1：如果此DOM有jskit-val属性，则会记录该属性的值
- * 2：如果此DOM有jskit-txt属性，则会记录该DOM的innerText
- * 3：如果此DOM有jskit-html属性，则会记录该DOM的innerHTML
- * 4：如果此DOM有jskit-count属性，则会记录与该DOM相同【jskit-key】的DOM元素的出现次数
- * 5：如果此DOM只有jskit-key属性，则会默认记录一个标记为"1"
+ * 1：如果此DOM的 jskit-type="val"，则会记录该属性的值
+ * 2：如果此DOM的 jskit-type="txt"，则会记录该DOM的innerText
+ * 3：如果此DOM的 jskit-type="html"，则会记录该DOM的innerHTML
+ * 4：如果此DOM的 jskit-type="count"，则会记录与该DOM相同【jskit-key】的DOM元素的出现次数
+ * 5：如果此DOM没有 jskit-type属性，则会默认记录一个标记为"1"
  */
 export class DOMDataRecorder {
     /**
@@ -50,10 +77,7 @@ export class DOMDataRecorder {
         this.refer = document.referrer
         //数据
         const attrKeyName = `${this.attrPrefix}key`
-        const attrValueName = `${this.attrPrefix}val`
-        const attrTxtName = `${this.attrPrefix}txt`
-        const attrHtmlName = `${this.attrPrefix}html`
-        const attrCountName = `${this.attrPrefix}count`
+        const attrTypeName = `${this.attrPrefix}type`
         const eles = document.querySelectorAll(`[${attrKeyName}]`)
         if (!eles || !eles.length) {
             return this
@@ -63,7 +87,22 @@ export class DOMDataRecorder {
             if (!key) {
                 return
             }
+            const type = (node.getAttribute(attrTypeName) || "").toLowerCase().trim()
             this.value[key] = this.value[key] || []
+            switch (type) {
+                case KeyTypeEnum.txt:
+                    break;
+                case KeyTypeEnum.html:
+                    break;
+                case KeyTypeEnum.group:
+                    break;
+                case KeyTypeEnum.count:
+                    break;
+                case KeyTypeEnum.val:
+                default:
+                    break;
+            }
+
             //如果存在jskit-val属性，则记录此属性值
             if (node.hasAttribute(attrValueName)) {
                 this.value[key].push((node.getAttribute(attrValueName) || "").trim())
