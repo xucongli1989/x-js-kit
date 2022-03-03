c9d4d99ca3fc70811ad4acd7c5733838745bf7f9
xucongli1989 <80213876@qq.com>
2022-03-03 14:53:32 +0800

    build: build

--- lib/common/regexp.d.ts
+++ lib/common/regexp.d.ts
@@ -1,5 +1,12 @@
 /**
+ * 正则引擎枚举
+ */
+export declare enum RegExpEngineEnum {
+    ECMA = "ECMA",
+    DOTNET = "DOTNET"
+}
+/**
  * 将字符串中的关键字符进行编码，以避免在传给Regex时，这些字符被当成正则中的关键字处理
  */
-export declare function escapeReg(str: string): string;
+export declare function escapeReg(str: string, engine?: RegExpEngineEnum): string;
 //# sourceMappingURL=regexp.d.ts.map
