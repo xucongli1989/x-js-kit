66c3dfb4e959bbf1da2fc6004bf0b51537b07421
xucongli1989 <80213876@qq.com>
2022-05-23 13:44:47 +0800

    chore: 删除无用代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 const config = {
     entry: {
@@ -34,20 +32,10 @@ const config = {
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: [path.join(process.cwd(), './{dist,lib}/*')]
         }),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            excludeChunks: ["index"]//只引用polyfill的版本即可
-        }),
-        new HtmlWebpackTagsPlugin({ tags: [], append: false }), //向html中追加元素，暂时没有用到
         new webpack.HotModuleReplacementPlugin()
     ],
     optimization: {
         minimize: true
-    },
-    devServer: {
-        inline: true,
-        open: true,
-        contentBase: path.join(__dirname, "./dist")
     }
 };
 module.exports = config;
