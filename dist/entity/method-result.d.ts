7609065f5a489c9d8c9c4959baa05e41de13fe21
xucongli1989 <80213876@qq.com>
2022-09-13 15:37:45 +0800

    chore: update entity

--- dist/entity/method-result.d.ts
+++ dist/entity/method-result.d.ts
@@ -1,5 +1,5 @@
 /**
- * 消息结果实体
+ * 消息结果实体（首字母小写）
  */
 export declare class MethodResult<TResult = any, TData = any> {
     /**
@@ -22,5 +22,30 @@ export declare class MethodResult<TResult = any, TData = any> {
      * 其它数据（比如用dictionary存放不同的数据结果）
      */
     data: TData;
+}
+/**
+ * 消息结果实体（首字母大写）
+ */
+export declare class MethodResult2<TResult = any, TData = any> {
+    /**
+     * 该方法执行的逻辑是否成功（默认为true）
+     */
+    IsSuccess: boolean;
+    /**
+     * 该方法返回的消息提示
+     */
+    Message: string;
+    /**
+     * 该方法返回的错误消息提示
+     */
+    ErrorMessage: string;
+    /**
+     * 该方法返回的结果
+     */
+    Result: TResult;
+    /**
+     * 其它数据（比如用dictionary存放不同的数据结果）
+     */
+    Data: TData;
 }
 //# sourceMappingURL=method-result.d.ts.map
--- src/entity/method-result.ts
+++ src/entity/method-result.ts
@@ -1,5 +1,5 @@
 /**
- * 消息结果实体
+ * 消息结果实体（首字母小写）
  */
 export class MethodResult<TResult = any, TData = any> {
     /**
@@ -22,4 +22,30 @@ export class MethodResult<TResult = any, TData = any> {
      * 其它数据（比如用dictionary存放不同的数据结果）
      */
     data: TData = undefined as any
+}
+
+/**
+ * 消息结果实体（首字母大写）
+ */
+export class MethodResult2<TResult = any, TData = any> {
+    /**
+     * 该方法执行的逻辑是否成功（默认为true）
+     */
+    IsSuccess: boolean = true
+    /**
+     * 该方法返回的消息提示
+     */
+    Message: string = ""
+    /**
+     * 该方法返回的错误消息提示
+     */
+    ErrorMessage: string = ""
+    /**
+     * 该方法返回的结果
+     */
+    Result: TResult = undefined as any
+    /**
+     * 其它数据（比如用dictionary存放不同的数据结果）
+     */
+    Data: TData = undefined as any
 }
